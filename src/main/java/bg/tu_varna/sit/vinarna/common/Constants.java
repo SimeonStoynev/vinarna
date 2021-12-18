2361a043c8e8d620a93c961c2e87e0e2060fb6de
Tsvetelin <cvetelin626@gmail.com>
2021-12-18 18:09:10 +0200

    Edited Resources/presentations/views dir

--- src/main/java/bg/tu_varna/sit/vinarna/common/Constants.java
+++ src/main/java/bg/tu_varna/sit/vinarna/common/Constants.java
@@ -2,6 +2,6 @@ package bg.tu_varna.sit.vinarna.common;
 
 public class Constants {
     public static class View {
-        public static final String HELLO_VIEW = "/bg/tu_varna/sit/vinarna/presentation.views/hello-view.fxml";
+        public static final String HELLO_VIEW = "/bg/tu_varna/sit/vinarna/presentation/views/hello-view.fxml";
     }
 }
