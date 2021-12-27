a8677ca791bc7e42d1a18c88f0272029102cad81
Tsvetelin <cvetelin626@gmail.com>
2021-12-27 22:53:55 +0200

    Users table reload is now in separated method

--- src/main/java/bg/tu_varna/sit/vinarna/presentation/controllers/UsersAnchorPaneController.java
+++ src/main/java/bg/tu_varna/sit/vinarna/presentation/controllers/UsersAnchorPaneController.java
@@ -26,35 +26,44 @@ public class UsersAnchorPaneController {
     AnchorPane userRowsAnchorPane;
 
     @FXML
-    private void initialize() throws IOException {
-        List<User> users = repository.getAll();
-        userRowsAnchorPane.getChildren().remove(0);
-        int y = 0;
-        boolean bg = false;
-        for(User user : users) {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/bg/tu_varna/sit/vinarna/presentation/views/Users/UsersTableRow.fxml"));
-            AnchorPane userRow = loader.load();
-            UsersTableRowController controller = loader.getController();
+    private void initialize() {
+        userTableViewReload();
+    }
 
-            controller.idLabel.setText(String.valueOf(user.getId()));
-            controller.usernameLabel.setText(user.getUsername());
-            controller.firstNameLabel.setText(user.getFirstName());
-            controller.lastNameLabel.setText(user.getLastName());
-            controller.emailLabel.setText(user.getEmail());
-            controller.phoneLabel.setText(user.getPhone());
-            controller.roleLabel.setText(user.getRole().getName());
-            if(user.getRole().getId() == 1)
-                controller.settingsMenuButton.setVisible(false);
+    public void userTableViewReload() {
+        try {
+            List<User> users = repository.getAll();
+            userRowsAnchorPane.getChildren().clear();
+            int y = 0;
+            boolean bg = false;
+            for(User user : users) {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/bg/tu_varna/sit/vinarna/presentation/views/Users/UsersTableRow.fxml"));
+                AnchorPane userRow = loader.load();
+                UsersTableRowController controller = loader.getController();
 
-            AnchorPane.setRightAnchor(userRow, 0.0);
-            AnchorPane.setLeftAnchor(userRow, 0.0);
-            userRow.setLayoutY(y);
-            if(bg)
-                userRow.setStyle("-fx-background-color: rgba(66,65,54,0.21);");
+                controller.idLabel.setText(String.valueOf(user.getId()));
+                controller.usernameLabel.setText(user.getUsername());
+                controller.firstNameLabel.setText(user.getFirstName());
+                controller.lastNameLabel.setText(user.getLastName());
+                controller.emailLabel.setText(user.getEmail());
+                controller.phoneLabel.setText(user.getPhone());
+                controller.roleLabel.setText(user.getRole().getName());
+                if(user.getRole().getId() == 1)
+                    controller.settingsMenuButton.setVisible(false);
 
-            userRowsAnchorPane.getChildren().add(userRow);
-            y+=45;
-            bg=!bg;
+                AnchorPane.setRightAnchor(userRow, 0.0);
+                AnchorPane.setLeftAnchor(userRow, 0.0);
+                userRow.setLayoutY(y);
+                if(bg)
+                    userRow.setStyle("-fx-background-color: rgba(66,65,54,0.21);");
+
+                userRowsAnchorPane.getChildren().add(userRow);
+                y+=45;
+                bg=!bg;
+            }
+        } catch (Exception ex) {
+            log.error("User table reload error: " + ex.getMessage());
         }
+
     }
 }
