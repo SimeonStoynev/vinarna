103701d601e3a9a0a74c5c21c0cb29f65e884b61
Tsvetelin <cvetelin626@gmail.com>
2021-12-27 22:28:42 +0200

    The users are loading from the DB now

--- src/main/java/bg/tu_varna/sit/vinarna/presentation/controllers/UsersAnchorPaneController.java
+++ src/main/java/bg/tu_varna/sit/vinarna/presentation/controllers/UsersAnchorPaneController.java
@@ -1,5 +1,7 @@
 package bg.tu_varna.sit.vinarna.presentation.controllers;
 
+import bg.tu_varna.sit.vinarna.data.entities.User;
+import bg.tu_varna.sit.vinarna.data.repositories.UserRepository;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.TableColumn;
@@ -10,9 +12,12 @@ import javafx.scene.layout.Region;
 import org.apache.log4j.Logger;
 
 import java.io.IOException;
+import java.util.List;
 
 public class UsersAnchorPaneController {
     private static final Logger log = Logger.getLogger(UsersAnchorPaneController.class);
+
+    public final UserRepository repository = UserRepository.getInstance();
 
     @FXML
     AnchorPane mainAnchorPanel;
@@ -22,24 +27,30 @@ public class UsersAnchorPaneController {
 
     @FXML
     private void initialize() throws IOException {
-
-
+        List<User> users = repository.getAll();
         userRowsAnchorPane.getChildren().remove(0);
         int y = 0;
         boolean bg = false;
-        for(int i = 0; i < 100; i++) {
+        for(User user : users) {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/bg/tu_varna/sit/vinarna/presentation/views/Users/UsersTableRow.fxml"));
-            AnchorPane user = loader.load();
+            AnchorPane userRow = loader.load();
             UsersTableRowController controller = loader.getController();
-            controller.idLabel.setText(String.valueOf(i));
 
-            AnchorPane.setRightAnchor(user, 0.0);
-            AnchorPane.setLeftAnchor(user, 0.0);
-            user.setLayoutY(y);
+            controller.idLabel.setText(String.valueOf(user.getId()));
+            controller.usernameLabel.setText(user.getUsername());
+            controller.firstNameLabel.setText(user.getFirstName());
+            controller.lastNameLabel.setText(user.getLastName());
+            controller.emailLabel.setText(user.getEmail());
+            controller.phoneLabel.setText(user.getPhone());
+            controller.roleLabel.setText(user.getRole().getName());
+
+            AnchorPane.setRightAnchor(userRow, 0.0);
+            AnchorPane.setLeftAnchor(userRow, 0.0);
+            userRow.setLayoutY(y);
             if(bg)
-                user.setStyle("-fx-background-color: rgba(66,65,54,0.21);");
+                userRow.setStyle("-fx-background-color: rgba(66,65,54,0.21);");
 
-            userRowsAnchorPane.getChildren().add(user);
+            userRowsAnchorPane.getChildren().add(userRow);
             y+=45;
             bg=!bg;
         }
