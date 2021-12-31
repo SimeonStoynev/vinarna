110f231fe9be40c2eab7557b88be5e99401bc201
Tsvetelin <cvetelin626@gmail.com>
2021-12-31 02:12:04 +0200

    Implemented WineTypeModel

---
+++ src/main/java/bg/tu_varna/sit/vinarna/presentation/models/WineTypeModel.java
@@ -0,0 +1,91 @@
+package bg.tu_varna.sit.vinarna.presentation.models;
+
+import bg.tu_varna.sit.vinarna.data.entities.WineType;
+
+import java.sql.Timestamp;
+import java.util.Objects;
+
+public class WineTypeModel {
+    private int id;
+    private String name;
+    private Timestamp created_at;
+    private Timestamp updated_at;
+
+    public WineTypeModel() {}
+
+    public WineTypeModel(int id, String name, Timestamp created_at, Timestamp updated_at) {
+        this.id = id;
+        this.name = name;
+        this.created_at = created_at;
+        this.updated_at = updated_at;
+    }
+
+    public WineTypeModel(WineTypeModel wineType) {
+        this.id = wineType.getId();
+        this.name = wineType.getName();
+        this.created_at = wineType.getCreated_at();
+        this.updated_at = wineType.getUpdated_at();
+    }
+
+    public WineTypeModel(WineType wineType) {
+        this.id = wineType.getId();
+        this.name = wineType.getName();
+        this.created_at = wineType.getCreated_at();
+        this.updated_at = wineType.getUpdated_at();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Timestamp getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Timestamp created_at) {
+        this.created_at = created_at;
+    }
+
+    public Timestamp getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(Timestamp updated_at) {
+        this.updated_at = updated_at;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof WineTypeModel)) return false;
+        WineTypeModel that = (WineTypeModel) o;
+        return id == that.id && name.equals(that.name) && created_at.equals(that.created_at) && updated_at.equals(that.updated_at);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, created_at, updated_at);
+    }
+
+    @Override
+    public String toString() {
+        return "WineTypeModel{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", created_at=" + created_at +
+                ", updated_at=" + updated_at +
+                '}';
+    }
+}
